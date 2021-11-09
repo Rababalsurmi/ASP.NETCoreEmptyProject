ab2bf38ea1fc6f22174b429d50b81abb1b6ed697
rababalsurmi <rababalsurmi@gmail.com>
2021-11-09 14:08:59 +0100

    Empty Project
    Created empty project but it is not working

---
+++ ASP.NETCoreEmptyProject/Models/ErrorViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+
+using System;
+
+namespace ASP.NET_Core_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
