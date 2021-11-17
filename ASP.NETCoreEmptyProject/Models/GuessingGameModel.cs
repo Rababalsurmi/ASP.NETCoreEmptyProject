9aaed98f146a137c663e8f15d7ef12f65cc5c6b2
rababalsurmi <rababalsurmi@gmail.com>
2021-11-17 16:06:58 +0100

    Update

--- ASP.NETCoreEmptyProject/Models/GuessingGameModel.cs
+++ ASP.NETCoreEmptyProject/Models/GuessingGameModel.cs
@@ -5,77 +5,45 @@ namespace ASP.NETCoreEmptyProject.Models
 {
     public class GuessingGameModel
     {
-        public int RndNum { get; set; }
-        public static Random random = new Random();
-        
+            public int RndNum { get; set; }
 
-        private int guessedNum;
-        public int GuessedNum { get { return guessedNum; } set { guessedNum = value; } }
-
-        public bool ShowResult { get; set; }
-        public bool Success { get; set; }
+            public int GuessedNum { get; set; }
 
 
-        public static string WriteMessage()
-        {
-            return "Guess a Number between 1 and 100! You have 5 tries!";
-        }
 
-        public string CheckNumber(int guessedNum)
-        {
-            string message = "";
-
-            bool run = true;
-            int allowedTries = 5;
-            int numberOfTries = 0;
-           
-            while (run)
+            public static string WriteGameMessage()
             {
-                int RndNum = random.Next(1, 100);
-               
-
-                while (true)
-                {
-                    allowedTries--;
-                    numberOfTries++;
-
-                    if (guessedNum == RndNum)
-                    {
-                        message = $"Your guess was correct!" + $"You used { numberOfTries} tries.";
-                       
-                        break;
-                    }
-                    else if (guessedNum > RndNum)
-                    {
-                        message = $"Your guess was too high." + $" You have {allowedTries} tries left. Enter another number: ";
-
-                        break;
-                    }
-                    else if (guessedNum < RndNum)
-                    {
-                        message = $"Your guess was too low." + $" You have {allowedTries} tries left. Enter another number: ";
-                        
-                        break;
-
-                    }
-                    else if (guessedNum != RndNum && allowedTries == 0)
-                    {
-                        message = $"Game Over! The number was: {RndNum}";
-                        break;
-                    }
-                    else
-                    {
-                        message = $"You have {allowedTries - numberOfTries} tries left. Enter another number: ";
-                        break;
-                    }
-                }
-               
-
-                run = false;
+                return "Guess a Number between 1 and 100!";
             }
 
-            return message ;
-        }
+            public void RandomNumber()
+            {
+                Random random = new Random();
+                RndNum = random.Next(1, 100);
+            }
+
+            public string Success()
+            {
+                string message = "Your guess was correct!";
+
+                return message;
+            }
+
+            public string WasLow()
+            {
+                string message = "Your guess was too low!";
+
+                return message;
+            }
+
+            public string WasHigh()
+            {
+                string message = "Your guess was too High!";
+
+                return message;
+            }
+
+           
         
     }
 }
