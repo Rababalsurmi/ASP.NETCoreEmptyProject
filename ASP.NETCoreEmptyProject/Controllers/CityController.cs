2b632eabbf69c6852917766fd27378017bc92fc7
rababalsurmi <rababalsurmi@gmail.com>
2021-12-06 11:11:29 +0100

    City Controller

---
+++ ASP.NETCoreEmptyProject/Controllers/CityController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NETCoreEmptyProject.Data;
+using ASP.NETCoreEmptyProject.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP.NETCoreEmptyProject.Controllers
+{
+    public class CityController : Controller
+    {
+
+        // GET: /<controller>/
+        private readonly PeopleDBContext _context;
+
+        public CityController(PeopleDBContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult City()
+        {
+            List<PeopleCityModel> ListOfCities = _context.PeopleCity.ToList();
+            return View(ListOfCities);
+        }
+    }
+}
